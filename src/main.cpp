10905ef42bdd5b2b8af6a2cf99ac6c7db6e2f9de
jhaseloh <joelina.haseloh@smail.th-koeln.de>
2023-02-21 17:33:54 +0100

    It works all

--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,22 @@ SoftwareSerial mySoftwareSerial(10, 11); // RX, TX
 DFRobotDFPlayerMini myDFPlayer;
 
 int pirPin = 12;
+int moisturePin = A0;
 
 int pirVal = 0;
 bool motionState = false; // We start with no motion detected.
+int moistureVal = 0;
+int moistureLimitValue = 500;
+int volume = 30; //Set volume value (0~30).
 
 void setup()
 {
   pinMode(pirPin, INPUT);
+  pinMode(moisturePin, INPUT);
   
   mySoftwareSerial.begin(9600);
   Serial.begin(115200);
-  
-  Serial.println();
-  Serial.println(F("DFRobot DFPlayer Mini Demo"));
+
   Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
   
   if (!myDFPlayer.begin(mySoftwareSerial, false)) {  //Use softwareSerial to communicate with mp3.
@@ -30,7 +33,7 @@ void setup()
   Serial.println(F("DFPlayer Mini online."));
   
   //----Set volume----
-  myDFPlayer.volume(10);  //Set volume value (0~30).
+  myDFPlayer.volume(volume);  //Set volume value (0~30).
   delay(1000);
   
  //----Read volume----
@@ -42,12 +45,16 @@ void setup()
 void loop()
 {
   pirVal = digitalRead(pirPin); // Read out the pirPin and store as val:
+  moistureVal = analogRead(moisturePin);
+  //Serial.println(moistureVal);
+
 
   // If motion is detected (pirPin = HIGH), do the following:
-  if (pirVal == HIGH) {
+  if (pirVal == HIGH && moistureVal > moistureLimitValue) {
     // Change the motion state to true (motion detected):
     if (motionState == false) {
       Serial.println("Motion detected!");
+      Serial.println(moistureVal);
       motionState = true;
       myDFPlayer.play(2);
     }
