fdf5682c9208c7f3b212050b65ecaa1a083d0066
jhaseloh <joelina.haseloh@smail.th-koeln.de>
2023-02-21 16:23:18 +0100

    Its not loud anymore

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
 /***************************************************
-DFPlayer - A Mini MP3 Player For Arduino
- <https://www.dfrobot.com/index.php?route=product/product&product_id=1121>
-
+ DFPlayer - A Mini MP3 Player For Arduino
+ <https://www.dfrobot.com/product-1121.html>
+ 
  ***************************************************
- This example shows the basic function of library for DFPlayer.
-
+ This example shows the all the function of library for DFPlayer.
+ 
  Created 2016-12-07
  By [Angelo qiao](Angelo.qiao@dfrobot.com)
-
+ 
  GNU Lesser General Public License.
  See <http://www.gnu.org/licenses/> for details.
  All above must be included in any redistribution
@@ -15,7 +15,7 @@ DFPlayer - A Mini MP3 Player For Arduino
 
 /***********Notice and Trouble shooting***************
  1.Connection and Diagram can be found here
- <https://www.dfrobot.com/wiki/index.php/DFPlayer_Mini_SKU:DFR0299#Connection_Diagram>
+<https://www.dfrobot.com/wiki/index.php/DFPlayer_Mini_SKU:DFR0299#Connection_Diagram>
  2.This code is tested on Arduino Uno, Leonardo, Mega boards.
  ****************************************************/
 
@@ -31,11 +31,11 @@ void setup()
 {
   mySoftwareSerial.begin(9600);
   Serial.begin(115200);
-
+  
   Serial.println();
   Serial.println(F("DFRobot DFPlayer Mini Demo"));
   Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
-
+  
   if (!myDFPlayer.begin(mySoftwareSerial, false)) {  //Use softwareSerial to communicate with mp3.
     Serial.println(F("Unable to begin:"));
     Serial.println(F("1.Please recheck the connection!"));
@@ -43,21 +43,96 @@ void setup()
     while(true);
   }
   Serial.println(F("DFPlayer Mini online."));
-
-  myDFPlayer.volume(1);  //Set volume value. From 0 to 30
+  
+  myDFPlayer.setTimeOut(500); //Set serial communictaion time out 500ms
+  //----Set volume----
+  myDFPlayer.volume(10);  //Set volume value (0~30).
+  delay(1000);
+  //myDFPlayer.volumeUp(); //Volume Up
   //myDFPlayer.volumeDown(); //Volume Down
+  
+  //----Set different EQ----
+  myDFPlayer.EQ(DFPLAYER_EQ_NORMAL);
+//  myDFPlayer.EQ(DFPLAYER_EQ_POP);
+//  myDFPlayer.EQ(DFPLAYER_EQ_ROCK);
+//  myDFPlayer.EQ(DFPLAYER_EQ_JAZZ);
+//  myDFPlayer.EQ(DFPLAYER_EQ_CLASSIC);
+//  myDFPlayer.EQ(DFPLAYER_EQ_BASS);
+  
+  //----Set device we use SD as default----
+//  myDFPlayer.outputDevice(DFPLAYER_DEVICE_U_DISK);
+  //myDFPlayer.outputDevice(DFPLAYER_DEVICE_SD);
+//  myDFPlayer.outputDevice(DFPLAYER_DEVICE_AUX);
+//  myDFPlayer.outputDevice(DFPLAYER_DEVICE_SLEEP);
+//  myDFPlayer.outputDevice(DFPLAYER_DEVICE_FLASH);
+  
+  //----Mp3 control----
+//  myDFPlayer.sleep();     //sleep
+//  myDFPlayer.reset();     //Reset the module
+//  myDFPlayer.enableDAC();  //Enable On-chip DAC
+//  myDFPlayer.disableDAC();  //Disable On-chip DAC
+//  myDFPlayer.outputSetting(true, 15); //output setting, enable the output and set the gain to 15
+  
+  //----Mp3 play----
+  /*myDFPlayer.next();  //Play next mp3
+  delay(1000);
+  myDFPlayer.previous();  //Play previous mp3
+  delay(1000);
   myDFPlayer.play(1);  //Play the first mp3
+  delay(1000);
+  myDFPlayer.loop(1);  //Loop the first mp3
+  delay(1000);
+  myDFPlayer.pause();  //pause the mp3
+  delay(1000);
+  myDFPlayer.start();  //start the mp3 from the pause
+  delay(1000);
+  myDFPlayer.playFolder(15, 4);  //play specific mp3 in SD:/15/004.mp3; Folder Name(1~99); File Name(1~255)
+  delay(1000);
+  myDFPlayer.enableLoopAll(); //loop all mp3 files.
+  delay(1000);
+  myDFPlayer.disableLoopAll(); //stop loop all mp3 files.
+  delay(1000);
+  myDFPlayer.playMp3Folder(4); //play specific mp3 in SD:/MP3/0004.mp3; File Name(0~65535)
+  delay(1000);
+  myDFPlayer.advertise(3); //advertise specific mp3 in SD:/ADVERT/0003.mp3; File Name(0~65535)
+  delay(1000);
+  myDFPlayer.stopAdvertise(); //stop advertise
+  delay(1000);
+  myDFPlayer.playLargeFolder(2, 999); //play specific mp3 in SD:/02/004.mp3; Folder Name(1~10); File Name(1~1000)
+  delay(1000);
+  myDFPlayer.loopFolder(5); //loop all mp3 files in folder SD:/05.
+  delay(1000);
+  myDFPlayer.randomAll(); //Random play all the mp3.
+  delay(1000);
+  myDFPlayer.enableLoop(); //enable loop.
+  delay(1000);
+  myDFPlayer.disableLoop(); //disable loop.
+  delay(1000);
+  */
+  //----Read imformation----
+  //Serial.println(myDFPlayer.readState()); //read mp3 state
+  delay(1000);
+  Serial.println("Volume:");
+  Serial.println(myDFPlayer.readVolume()); //read current volume
+  myDFPlayer.volume(10);
+  delay(1000);
+  Serial.println("Volume:");
+  Serial.println(myDFPlayer.readVolume());
+  //Serial.println(myDFPlayer.readEQ()); //read EQ setting
+  //Serial.println(myDFPlayer.readFileCounts()); //read all file counts in SD card
+  //Serial.println(myDFPlayer.readCurrentFileNumber()); //read current play file number
+  //Serial.println(myDFPlayer.readFileCountsInFolder(3)); //read file counts in folder SD:/03
 }
 
 void loop()
 {
   static unsigned long timer = millis();
-
+  
   if (millis() - timer > 3000) {
     timer = millis();
     myDFPlayer.next();  //Play next mp3 every 3 second.
   }
-
+  
   if (myDFPlayer.available()) {
     printDetail(myDFPlayer.readType(), myDFPlayer.read()); //Print the detail message from DFPlayer to handle different errors and states.
   }
@@ -79,6 +154,12 @@ void printDetail(uint8_t type, int value){
       break;
     case DFPlayerCardOnline:
       Serial.println(F("Card Online!"));
+      break;
+    case DFPlayerUSBInserted:
+      Serial.println("USB Inserted!");
+      break;
+    case DFPlayerUSBRemoved:
+      Serial.println("USB Removed!");
       break;
     case DFPlayerPlayFinished:
       Serial.print(F("Number:"));
@@ -116,4 +197,5 @@ void printDetail(uint8_t type, int value){
     default:
       break;
   }
+  
 }
